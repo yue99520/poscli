ccad9acd7512924b2d263dc5a08b92a4ca92b626
ernie <dy99520@gmail.com>
2021-04-25 16:37:32 +0800

    [dev] Log

---
+++ apps/log.py
@@ -0,0 +1,22 @@
+import logging
+import datetime
+
+
+def logger_init(log_level):
+    t = datetime.datetime.now()
+    _filename = "log_{Y}_{M}_{D}_{h}_{m}_{s}".format(
+        Y=str(t.year),
+        M=str(t.month),
+        D=str(t.day),
+        h=str(t.hour),
+        m=str(t.min),
+        s=str(t.second),
+    )
+
+    logging.basicConfig(
+        level=log_level,
+        filemode='w',
+        format='%(levelname)-8s | %(asctime)s | %(module)s:%(lineno)d | %(message)s'
+    )
+
+    logging.info("Logger initialized. level=" + logging.getLevelName(log_level))
